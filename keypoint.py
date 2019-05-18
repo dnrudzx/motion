2d388ae6327989dda4b48c86c3d4724d3090dac8
applegarden <vksxkwl65@gmail.com>
2019-05-18 16:23:26 +0900

    5/18

--- keypoint.py
+++ keypoint.py
@@ -11,7 +11,7 @@ def NOSE(num):
     #npy 파일을 불러오고 거기서 nose의 값만 가져와 새로운 배열을 만든다
     while True:
         try:
-            arr = np.load("../test/array_"+ num +"%d.npy"% count)
+            arr = np.load("/home/ms/test/array_"+ num +"%d.npy"% count)
         except FileNotFoundError:
             break
     
@@ -21,35 +21,32 @@ def NOSE(num):
                
         else:
             try:
-                NO = np.insert(NO, count, a[0, 0], axis=0)        
+                NO = np.insert(NO, count, arr[0, 0], axis=0)        
             except IndexError:
                 NO = np.insert(NO, count, 0, axis=0)        
         count += 1
     #코 배열을 가져와 x값을 x배열에 넣는다
     #값이 0 이면 0을 넣어주고 x값이 가로 크기/2 보다 크면 머리가 왼쪽으로 오게 바꿔준다
     x = []
-        for i in range(0, len(NO)):
-            if NO[i,0] == 0:
-                x = np.insert(x, i, 0)
-                continue
-            else:
-                x = np.insert(x, i, int(NO[i,0]))
+    for i in range(0, len(NO)):
+        if NO[i,0] == 0:
+            x = np.insert(x, i, 0)
+            continue
+        else:
+            x = np.insert(x, i, int(NO[i,0]))
 
         #코 배열을 가져와 y값을 y배열에 넣는다
-        y = []
-        for i in range(0, len(NO)):
-            if NO[i,1] == 0:
-                y = np.insert(y, i, 0)
-                continue
-            y = np.insert(y, i, abs(NO[i,1]))
+    y = []
+    for i in range(0, len(NO)):
+        if NO[i,1] == 0:
+            y = np.insert(y, i, 0)
+            continue
+        y = np.insert(y, i, abs(NO[i,1]))
 
     return x , y
 
 
-
-
-
-    '''
+'''
     NE = np.zeros((1,3))
     RS = np.zeros((1,3))
     RE = np.zeros((1,3))
@@ -64,4 +61,4 @@ def NOSE(num):
     LH = np.zeros((1,3))
     LK = np.zeros((1,3))
     LA = np.zeros((1,3))
-    '''
+'''
